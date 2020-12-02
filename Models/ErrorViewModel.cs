c4c2f5afcd264979ef6c520bf9e18eeaff825cce
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-12-02 13:12:21 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EnterpriseSystemsDevelopment_EPortfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
