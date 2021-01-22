b63073168969b8c11943c4834cb7c5aa242dccff
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-22 15:29:59 +0000

    Annotations added to Apprentice model

--- Models/Apprentice.cs
+++ Models/Apprentice.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,13 +9,42 @@ namespace EnterpriseSystemsDevelopment_EPortfolio.Models
 {
     public class Apprentice
     {
+		[StringLength(50)]
 		private int apprenticeID { get; set; }
+
+		[Required]
+		[StringLength(20)]
+		[DisplayName("First Name")]
 		private String firstName { get; set; }
+
+		[Required]
+		[StringLength(20)]
+		[DisplayName("Last Name")]
 		private String lastName { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		[DisplayName("Specialism")]
 		private String specialismName { get; set; }
+
+		[Required]
+		[StringLength(20)]
+		[DisplayName("Job Title")]
 		private String jobTitle { get; set; }
+
+		[Required]
+		[StringLength(20)]
+		[DisplayName("Employer")]
 		private String employer { get; set; }
+
+		[Required]
+		[StringLength(20)]
+		[DataType(DataType.EmailAddress)]
 		private String email { get; set; }
+
+		[Required]
+		[StringLength(20)]
+		[DataType(DataType.Password)]
 		private String password { get; set; }
 
 	}
