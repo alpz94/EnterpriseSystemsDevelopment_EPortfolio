0d49d522796e6300552198c91931636bbae2ce6f
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-22 16:05:15 +0000

    Added annotations to Specialism model

--- Models/Specialism.cs
+++ Models/Specialism.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +9,18 @@ namespace EnterpriseSystemsDevelopment_EPortfolio.Models
 {
     public class Specialism
     {
+        [Range(0, 15, ErrorMessage = "Can only be between 0 .. 15")]
         private int specialismID { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        [DisplayName("Specialism Name")]
         private String name { get; set; }
+
+        [Required]
+        [StringLength(4000)]
+        [DisplayName("Description")]
+        [DataType(DataType.MultilineText)]
         private String description { get; set; }
 
     }
