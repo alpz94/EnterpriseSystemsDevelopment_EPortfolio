a473d779f0502109f0d9207f4cfbbc554b1b971d
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-22 15:31:34 +0000

    Annotations added to Employer model

--- Models/Employer.cs
+++ Models/Employer.cs
@@ -7,7 +7,12 @@ namespace EnterpriseSystemsDevelopment_EPortfolio.Models
 {
     public class Employer
     {
+        [StringLength(50)]
         private int employerID { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        [DisplayName("Employer Name")]
         private String name { get; set; }
     }
 }
