e1e14f84068f15fe689f71d9027df87eac38339c
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-28 16:58:43 +0000

    Added Identity files in the Areas folder

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EnterpriseSystemsDevelopment_EPortfolio.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EnterpriseSystemsDevelopment_EPortfolio.Areas.Identity.IdentityHostingStartup))]
+namespace EnterpriseSystemsDevelopment_EPortfolio.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
