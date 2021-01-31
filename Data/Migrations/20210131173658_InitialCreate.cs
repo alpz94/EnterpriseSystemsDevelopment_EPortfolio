059680ac784ad25503115e4b6c061aaf63752bb5
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-31 18:32:34 +0000

    Added InitialCreate migration

---
+++ Data/Migrations/20210131173658_InitialCreate.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EnterpriseSystemsDevelopment_EPortfolio.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Template",
+                columns: table => new
+                {
+                    TemplateId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 20, nullable: false),
+                    Evidence = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Template", x => x.TemplateId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "KSBs",
+                columns: table => new
+                {
+                    KsbId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 20, nullable: false),
+                    Description = table.Column<string>(maxLength: 4000, nullable: false),
+                    TemplateId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KSBs", x => x.KsbId);
+                    table.ForeignKey(
+                        name: "FK_KSBs_Template_TemplateId",
+                        column: x => x.TemplateId,
+                        principalTable: "Template",
+                        principalColumn: "TemplateId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_KSBs_TemplateId",
+                table: "KSBs",
+                column: "TemplateId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "KSBs");
+
+            migrationBuilder.DropTable(
+                name: "Template");
+        }
+    }
+}
