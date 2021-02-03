243add3e92347ee0cfccfdb6efc576aac9a43a0b
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-02-03 19:30:49 +0000

    Added KsbKeys enumeration; added home page content; minor changes in the templates table

---
+++ Models/KsbKeys.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnterpriseSystemsDevelopment_EPortfolio.Models
+{
+    public enum KsbKeys
+    {
+        SE1,
+        SE2,
+        SE3,
+        SE4,
+        SE5,
+        SE6,
+        SE7,
+        SE8,
+        SE9,
+        SE10,
+        SE11,
+        SE12,
+        SE13,
+        KE1,
+        KE2,
+        KE3,
+        KE4,
+        KE5,
+        KE6,
+        KE7,
+        KE8,
+        KE9,
+        KE10,
+        KE11,
+        KE12,
+        KE13,
+        KE14,
+        KE15,
+        KE16,
+        BE1,
+        BE2,
+        BE3,
+        BE4,
+        BE5,
+        BE6,
+        BE7,
+        BE8,
+        BE9,
+        BE10,
+        BE11,
+        BE12,
+        BE13,
+        BE14
+    }
+}
