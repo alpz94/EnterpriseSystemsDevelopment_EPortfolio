5240580520d1e3cceb613ef53b49b41b41a177e6
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-02-06 10:58:55 +0000

    Retrieve the KSB description; Added extentionMethods; Added KsbType enum

---
+++ Models/ExtentionMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnterpriseSystemsDevelopment_EPortfolio.Models
+{
+    public static class ExtentionMethods
+    {
+        public static string KsbDescription(this KsbKeys e)
+        {
+            switch (e)
+            {
+                case KsbKeys.SE1:
+                    return "Description SE1";
+                case KsbKeys.SE2:
+                    return "Description SE2";
+                case KsbKeys.SE3:
+                    return "Description SE3";
+            }
+            return "KSB error";
+        }
+    }
+}
---
+++ Models/KsbType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnterpriseSystemsDevelopment_EPortfolio.Models
+{
+    public enum KsbType
+    {
+        Skills,
+        Knowledge,
+        Behaviour
+    }
+}
