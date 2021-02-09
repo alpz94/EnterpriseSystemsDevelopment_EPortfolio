9b6a0825b090cff8139c613e9289c4d0992830f8
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-02-09 13:14:55 +0000

    data update

---
+++ Data/Migrations/20210209131429_dataUpdate.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EnterpriseSystemsDevelopment_EPortfolio.Data.Migrations
+{
+    public partial class dataUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Evidence_Template_TemplateId",
+                table: "Evidence");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TemplateId",
+                table: "Evidence",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Evidence_Template_TemplateId",
+                table: "Evidence",
+                column: "TemplateId",
+                principalTable: "Template",
+                principalColumn: "TemplateId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Evidence_Template_TemplateId",
+                table: "Evidence");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TemplateId",
+                table: "Evidence",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Evidence_Template_TemplateId",
+                table: "Evidence",
+                column: "TemplateId",
+                principalTable: "Template",
+                principalColumn: "TemplateId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
